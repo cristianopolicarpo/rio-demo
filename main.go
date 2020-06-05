05904a51c6fdde689868dfc531f69b3bbf9bbaf1
Cristiano Policarpo <cristiano.policarpo@baloom.co>
2020-06-05 00:14:09 -0300

    Update main.go

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 func handler(w http.ResponseWriter, r *http.Request) {
 	// Arbitrary sleep so that we can demonstrate autoscaler
 	time.Sleep(101 * time.Millisecond)
-	fmt.Fprintln(w, "Hi there, I'm running in Rio v1.0")
+	fmt.Fprintln(w, "Hi there, I'm running in Rio v1.1")
 }
 
 func main() {
